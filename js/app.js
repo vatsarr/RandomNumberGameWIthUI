09b4daf6c71436947d27ce66b63264b75cb010b0
Risto Vatsar <r.vatsar@gmail.com>
2023-03-07 12:31:27 +0200

    user tries added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Generate a random number between 1 and 100
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+let maxTries = 10;
+let userTries = 0;
 
 // Get DOM elements
 const guessInput = document.getElementById("guess-input");
@@ -10,23 +12,55 @@ const resultDiv = document.getElementById("result");
 guessBtn.addEventListener("click", function () {
   // Get user's guess and convert it to a number
   const guess = parseInt(guessInput.value);
-
   // Check if the guess is a number between 1 and 100
   if (isNaN(guess) || guess < 1 || guess > 100) {
     resultDiv.innerHTML = "Please enter a number between 1 and 100.";
     resultDiv.className = "error";
     return;
   }
+  userTries++;
 
   // Check if the guess is correct, too high, or too low
   if (guess === randomNumber) {
-    resultDiv.innerHTML = `Congratulations! You guessed the number (${randomNumber})!`;
+    resultDiv.innerHTML = `Congratulations! You guessed the number (${randomNumber}) in ${userTries} tries!`;
     resultDiv.className = "success";
+    guessInput.disabled = true;
+    guessBtn.disabled = true;
+    reloadBTN();
   } else if (guess < randomNumber) {
-    resultDiv.innerHTML = "Too low, try again.";
+    resultDiv.innerHTML = `Too low, try again. You have ${
+      maxTries - userTries
+    } tries left.`;
     resultDiv.className = "error";
   } else if (guess > randomNumber) {
-    resultDiv.innerHTML = "Too high, try again.";
+    resultDiv.innerHTML = `Too high, try again. You have ${
+      maxTries - userTries
+    } tries left.`;
     resultDiv.className = "error";
   }
+
+  if (userTries === maxTries) {
+    resultDiv.innerHTML = `Sorry, you didn't guess the number in time. The correct number was (${randomNumber}).`;
+    resultDiv.className = "error";
+    guessInput.disabled = true;
+    guessBtn.disabled = true;
+    reloadBTN();
+  }
 });
+
+function reloadBTN() {
+  // create button element
+  let refreshButton = document.createElement("button");
+
+  // set button text
+  refreshButton.textContent = "Play again!";
+  refreshButton.className = "reload-btn";
+
+  // add event listener to button
+  refreshButton.addEventListener("click", function () {
+    location.reload();
+  });
+
+  // add button to the page
+  document.body.appendChild(refreshButton);
+}
