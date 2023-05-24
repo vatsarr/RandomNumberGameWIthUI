fc22bbfbdf7a245531fbc5edaeed483af336806b
Risto Vatsar <r.vatsar@gmail.com>
2023-05-24 21:58:57 +0300

    added timer and some desing

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 let maxTries = 10;
 let userTries = 0;
+let sec = 0;
 
 // Get DOM elements
+const inputContainer = document.getElementById("guess-container");
 const guessInput = document.getElementById("guess-input");
+const triesLeft = document.getElementById("tries-left");
 const guessBtn = document.getElementById("guess-btn");
 const resultDiv = document.getElementById("result");
 const guessesContainer = document.getElementById("guessList");
@@ -23,30 +26,43 @@ guessBtn.addEventListener("click", function () {
 
   // Check if the guess is correct, too high, or too low
   if (guess === randomNumber) {
-    resultDiv.innerHTML = `Congratulations! You guessed the number (${randomNumber}) in ${userTries} tries!`;
+    resultDiv.innerHTML =
+      "&#127881" +
+      ` Congratulations! ` +
+      "&#127881" +
+      "</br>" +
+      `${randomNumber} was the correct number!` +
+      "</br>" +
+      `You guessed it in ${userTries} tries.`;
     resultDiv.className = "success";
     guessInput.disabled = true;
     guessBtn.disabled = true;
+    guessesContainer.remove();
+    clearInterval(timer);
     reloadBTN();
   } else if (guess < randomNumber) {
-    resultDiv.innerHTML = `Too low, try again. You have ${
-      maxTries - userTries
-    } tries left.`;
-    resultDiv.className = "error";
+    resultDiv.innerHTML = "Too low, try again.";
+    triesLeft.innerHTML = `${maxTries - userTries} tries left.`;
+    resultDiv.className = "warning";
     guessHistory();
   } else if (guess > randomNumber) {
-    resultDiv.innerHTML = `Too high, try again. You have ${
-      maxTries - userTries
-    } tries left.`;
-    resultDiv.className = "error";
+    resultDiv.innerHTML = "Too high, try again";
+    triesLeft.innerHTML = `${maxTries - userTries} tries left.`;
+    resultDiv.className = "warning";
     guessHistory();
   }
 
   if (userTries === maxTries) {
-    resultDiv.innerHTML = `Sorry, you didn't guess the number in time. The correct number was (${randomNumber}).`;
+    resultDiv.innerHTML =
+      `You ran out of tries!` +
+      "</br>" +
+      `The correct number was ${randomNumber}.`;
     resultDiv.className = "error";
     guessInput.disabled = true;
     guessBtn.disabled = true;
+    //guessesContainer.remove();
+    removeElements();
+    clearInterval(timer);
     reloadBTN();
   }
 });
@@ -68,6 +84,7 @@ function reloadBTN() {
   document.body.appendChild(refreshButton);
 }
 
+// Display guess history
 function guessHistory() {
   let guessesHeading = document.createElement("div");
   guessesHeading.className = "userGuesses";
@@ -76,5 +93,23 @@ function guessHistory() {
   let newGuess = document.createElement("li");
   newGuess.innerHTML = parseInt(guessInput.value);
   newGuess.className = "li-styling";
-  guessesContainer.appendChild(newGuess);
+  guessesHeading.appendChild(newGuess);
+}
+
+// Timer
+
+function pad(val) {
+  return val > 9 ? val : "0" + val;
+}
+let timer = setInterval(function () {
+  document.getElementById("seconds").innerHTML = pad(++sec % 60);
+  document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+}, 1000);
+
+// On win or loss remove some elements from page
+
+function removeElements() {
+  guessBtn.remove();
+  guessesContainer.remove();
+  triesLeft.remove();
 }
